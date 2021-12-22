542ab817f5c3f541d5fec3964f5d7409c8d4cd9d
Nver Khachatryan <78234170+NverKhachatryan@users.noreply.github.com>
2021-12-22 22:48:16 +0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,60 +1,181 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useEffect, useState} from 'react';
-import { ImageBackground, StyleSheet, Text, View, Image } from 'react-native';
-import * as Location from 'expo-location';
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, Image, Animated, PanResponder } from 'react-native';
 
-import DateTime from './components/DateTime';
-import WeatherScroll from './components/WeatherScroll';
+const SCREEN_HEIGHT = Dimensions.get('window').height
+const SCREEN_WIDTH = Dimensions.get('window').width
+import Icon from 'react-native-vector-icons/Ionicons'
+const Users = [
+  { id: "1", uri: require('./assets/1.jpeg') },
+  { id: "2", uri: require('./assets/2.jpeg') },
+  { id: "3", uri: require('./assets/3.jpeg') },
+  { id: "4", uri: require('./assets/4.jpeg') },
+  { id: "5", uri: require('./assets/5.jpeg') },
+]
 
-const API_KEY ='66c04c6c38e30bcf08501ea738aad84a';
-const img = require('./assets/image1.jpg')
+export default class App extends React.Component {
 
-export default function App() {
-  const [data, setData] = useState({});
+  constructor() {
+    super()
 
-  useEffect (() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        fetchDataFromApi("40.7128", "-74.0060")
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      fetchDataFromApi(location.coords.latitude, location.coords.longitude);
-    })();
-  }, [] )
-
-  const fetchDataFromApi = (latitude, longitude) => {
-    if(latitude && longitude) {
-      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
-
-      
-      setData(data)
-      })
+    this.position = new Animated.ValueXY()
+    this.state = {
+      currentIndex: 0
     }
-    
+
+    this.rotate = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH /2 ,0, SCREEN_WIDTH /2],
+      outputRange: ['-30deg', '0deg', '10deg'],
+      extrapolate: 'clamp'
+    })
+
+    this.rotateAndTranslate = {
+      transform: [{
+        rotate: this.rotate
+      },
+      ...this.position.getTranslateTransform()
+      ]
+    }
+
+    this.likeOpacity = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [0, 0, 1],
+      extrapolate: 'clamp'
+    })
+    this.dislikeOpacity = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [1, 0, 0],
+      extrapolate: 'clamp'
+    })
+
+    this.nextCardOpacity = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [1, 0, 1],
+      extrapolate: 'clamp'
+    })
+    this.nextCardScale = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [1, 0.8, 1],
+      extrapolate: 'clamp'
+    })
+
   }
-  return (
-    <View style={styles.container}>
-      <ImageBackground style={styles.image} source = {img} >
-        <DateTime current={data.current} timezone={data.timezone} lat={data.lat} lon={data.lon}/>
-        <WeatherScroll weatherData={data.daily}/>
-        
-      </ImageBackground>  
+  UNSAFE_componentWillMount() {
+    this.PanResponder = PanResponder.create({
 
-    </View>
-  );
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderMove: (evt, gestureState) => {
+
+        this.position.setValue({ x: gestureState.dx, y: gestureState.dy })
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+
+        if (gestureState.dx > 120) {
+          Animated.spring(this.position, {
+            toValue: { x: SCREEN_WIDTH + 100, y: gestureState.dy },
+            useNativeDriver: true,
+          }).start(() => {
+            this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
+              this.position.setValue({ x: 0, y: 0 })
+            })
+          })
+        }
+        else if (gestureState.dx < -120) {
+          Animated.spring(this.position, {
+            toValue: { x: -SCREEN_WIDTH - 100, y: gestureState.dy },
+            useNativeDriver: true,
+          }).start(() => {
+            this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
+              this.position.setValue({ x: 0, y: 0 })
+            })
+          })
+        }
+        else {
+          Animated.spring(this.position, {
+            toValue: { x: 0, y: 0 },
+            friction: 4,
+            useNativeDriver: true,
+          }).start()
+        }
+      }
+    })
+  }
+
+  renderUsers = () => {
+
+    return Users.map((item, i) => {
+
+
+      if (i < this.state.currentIndex) {
+        return null
+      }
+      else if (i == this.state.currentIndex) {
+
+        return (
+          <Animated.View
+            {...this.PanResponder.panHandlers}
+            key={item.id} style={[this.rotateAndTranslate, { height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding: 10, position: 'absolute' }]}>
+            <Animated.View style={{ opacity: this.likeOpacity, transform: [{ rotate: '-30deg' }], position: 'absolute', top: 50, left: 40, zIndex: 1000 }}>
+              <Text style={{ borderWidth: 1, borderColor: 'green', color: 'green', fontSize: 32, fontWeight: '800', padding: 10 }}>LIKE</Text>
+
+            </Animated.View>
+
+            <Animated.View style={{ opacity: this.dislikeOpacity, transform: [{ rotate: '30deg' }], position: 'absolute', top: 50, right: 40, zIndex: 1000 }}>
+              <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>NOPE</Text>
+
+            </Animated.View>
+
+            <Image
+              style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }}
+              source={item.uri} />
+
+          </Animated.View>
+        )
+      }
+      else {
+        return (
+          <Animated.View
+
+            key={item.id} style={[{
+              opacity: this.nextCardOpacity,
+              transform: [{ scale: this.nextCardScale }],
+              height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding: 10, position: 'absolute'
+            }]}>
+            <Animated.View style={{ opacity: 0, transform: [{ rotate: '-30deg' }], position: 'absolute', top: 50, left: 40, zIndex: 1000 }}>
+              <Text style={{ borderWidth: 1, borderColor: 'green', color: 'green', fontSize: 32, fontWeight: '800', padding: 10 }}>LIKE</Text>
+
+            </Animated.View>
+
+            <Animated.View style={{ opacity: 0, transform: [{ rotate: '30deg' }], position: 'absolute', top: 50, right: 40, zIndex: 1000 }}>
+              <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>NOPE</Text>
+
+            </Animated.View>
+
+            <Image
+              style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }}
+              source={item.uri} />
+
+          </Animated.View>
+        )
+      }
+    }).reverse()
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={{ height: 60 }}>
+
+        </View>
+        <View style={{ flex: 1 }}>
+          {this.renderUsers()}
+        </View>
+        <View style={{ height: 60 }}>
+
+        </View>
+
+
+      </View>
+
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    
-  },
-  image:{
-    flex: 1,
-    resizeMode: "cover",
-    alignItems: "center",
-  },
-});
